b92dd254d0ac52d96ae86a940e09375d65907a0f
DarrellDai <daishiqin1996@gmail.com>
2022-09-15 13:37:07 -0400

    Fixed the bugs in done_dict, so an action won't be made by a non-existing agent

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import gym
 from unity_wrappers.envs import MultiUnityWrapper
 from mlagents_envs.environment import UnityEnvironment
-import matplotlib.pyplot as plt
 
 env_path = 'D:/Unity Projects/Hide and Seek/Env/Hide and Seek'
 unity_env = UnityEnvironment(env_path)
@@ -9,10 +8,10 @@ unity_env = UnityEnvironment(env_path)
 env = MultiUnityWrapper(unity_env=unity_env, uint8_visual=True)
 obs = env.reset()
 # {0: agent 0's observation, 1: ...]
-for i in range(100):
+for i in range(10000):
     actions = {}
     # {0: agent 0's action, 1: ...]
     for id in env.action_space:
         actions[id] = env.action_space[id].sample()
-    info = env.step(actions)
+    obs_dict, reward_dict, done_dict, info_dict = env.step(actions)
 # unity_env.close()
