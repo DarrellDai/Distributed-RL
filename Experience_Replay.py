9d8bf8ab47693ed8e3c75b895f5ab7a4422d3a03
daish <daishiqin1996@gmail.com>
2022-09-30 00:51:43 -0400

    Change to multiple batches

--- Experience_Replay.py
+++ Experience_Replay.py
@@ -1,13 +1,15 @@
 from collections import deque
 import random
 import numpy as np
+from copy import deepcopy
+
 
 class Memory():
 
     def __init__(self, memsize, agent_ids):
         self.memsize = memsize
-        self.memory={}
-        self.agent_ids=agent_ids
+        self.memory = {}
+        self.agent_ids = agent_ids
         for id in agent_ids:
             self.memory[id] = deque(maxlen=self.memsize)
 
@@ -18,19 +20,24 @@ class Memory():
 
     def get_batch(self, bsize, time_step, agent_id):
         self.check_dimension()
-        batch = []
-        num_episode_left = bsize
-        while num_episode_left>0:
-            sampled_epsiodes = random.sample(self.memory[agent_id], num_episode_left)
-            for episode in sampled_epsiodes:
-                if len(episode)>=time_step:
-                    point = np.random.randint(0, len(episode) + 1 - time_step)
-                    batch.append(episode[point:point + time_step])
-                    num_episode_left-=1
-        return batch
+        batches = []
+        for _ in range(int(np.ceil(self.memsize / bsize))):
+            batches.append([])
+        sampled_idx = random.sample(range(self.memsize), self.memsize)
+        order = 0
+        for batch in batches:
+            while len(batch) < bsize and order < self.memsize:
+                if len(self.memory[agent_id][sampled_idx[order]]) >= time_step:
+                    point = np.random.randint(0, len(self.memory[agent_id][sampled_idx[order]]) + 1 - time_step)
+                    batch.append(self.memory[agent_id][sampled_idx[order]][point:point + time_step])
+                else:
+                    batch.append(self.memory[agent_id][sampled_idx[order]])
+                order += 1
+        return batches
+
     def check_dimension(self):
         for id in self.agent_ids:
             for episode in self.memory[id]:
                 for t in episode:
-                    if len(t[0][0].shape)!=3:
+                    if len(t[0][0].shape) != 3:
                         raise RuntimeError
