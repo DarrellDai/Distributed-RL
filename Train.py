d0d4a29733596732ec16089cb499fdc730f5d956
daish <daishiqin1996@gmail.com>
2022-10-01 13:35:32 -0400

    Add training from loading human play checkpoint

--- Train.py
+++ Train.py
@@ -2,7 +2,7 @@ from Pipeline import Pipeline
 from Experience_Replay import Memory
 import yaml
 
-with open("Config/Train_Nav_Server.yaml") as file:
+with open("Config/Train_Nav_Local.yaml") as file:
     param = yaml.safe_load(file)
 
 pipeline = Pipeline()
@@ -12,12 +12,15 @@ pipeline.initialize_model(cnn_out_size=param["cnn_out_size"], lstm_hidden_size=p
                           action_out_size=param["action_out_size"], atten_size=param["atten_size"],
                           device_idx=param["device_idx"])
 criterion, optimizer, target_model = pipeline.initialize_training(learning_rate=param["learning_rate"])
-memory = Memory(memsize=param["memory_size"], agent_ids=pipeline.agent_ids)
 
 if param["resume"]:
     target_model, optimizer, total_steps, start_episode, episode_count, epsilon, memory = pipeline.resume_training(
         checkpoint_to_load=param["checkpoint_to_load"], optimizer=optimizer, target_model=target_model)
 else:
+    if param["train_from_human_play"]:
+        target_model, optimizer, _, _, _, _, _ = pipeline.resume_training(
+            checkpoint_to_load=param["checkpoint_to_load"], optimizer=optimizer, target_model=target_model)
+    memory = Memory(memsize=param["memory_size"], agent_ids=pipeline.agent_ids)
     epsilon = param["initial_epsilon"]
     total_steps = 0
     episode_count = 0
@@ -31,7 +34,8 @@ pipeline.train(start_episode=start_episode, episode_count=episode_count, total_e
                target_model=target_model,
                memory=memory, batch_size=param["batch_size"], time_step=param["time_step"],
                learning_rate=param["learning_rate"],
-               target_update_freq=param["target_update_freq(steps)"], update_freq=param["update_freq"], optimizer=optimizer,
+               target_update_freq=param["target_update_freq(steps)"], update_freq=param["update_freq"],
+               optimizer=optimizer,
                criterion=criterion,
                performance_display_interval=param["performance_display_interval"],
                checkpoint_save_interval=param["checkpoint_save_interval"],
