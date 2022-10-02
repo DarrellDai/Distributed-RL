c0d5ed2e708f9692f7afda5bab5e52201d15e79e
daish <daishiqin1996@gmail.com>
2022-10-02 02:36:37 -0400

    Update Test

--- Test.py
+++ Test.py
@@ -6,10 +6,11 @@ with open("config/Test_Nav.yaml") as file:
     param = yaml.safe_load(file)
 args = base_input_parameters().parse_args()
 pipeline = Pipeline()
+pipeline.initialize_env(env_path=param["env_path"])
 pipeline.initialize_model(cnn_out_size=param["cnn_out_size"], lstm_hidden_size=param["lstm_hidden_size"],
                           action_shape=param["action_shape"],
                           action_out_size=param["action_out_size"], atten_size=param["atten_size"],
-                          device_idx=param["device_idx"], env_path=param["env_path"])
+                          device_idx=param["device_idx"])
 
 pipeline.test(max_steps=param["max_steps"], total_episodes=param["total_episodes"], checkpoint_to_load=param["checkpoint_to_load"], name_tensorboard=param["name_tensorboard"])
 pipeline.env.close()
