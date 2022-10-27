d0689a6b9bfd186acd969d240ab7866aca09170a
daish <daishiqin1996@gmail.com>
2022-10-27 01:23:37 -0400

    Add new human_play

--- Human_play.py
+++ Human_play.py
@@ -1,40 +1,43 @@
-import keyboard
 import numpy as np
-from unity_wrappers.envs import MultiUnityWrapper
-from mlagents_envs.environment import UnityEnvironment
-from utils import save_checkpoint
-from tqdm import tqdm
-from copy import deepcopy
-from Experience_Replay import Memory
-from datetime import datetime
-import yaml
-import os
 import torch
 
-with open("config/Human_Play.yaml") as file:
-    param = yaml.safe_load(file)
+import os
+from copy import deepcopy
+import redis
+from tqdm import tqdm
+from datetime import datetime
+import yaml
+import keyboard
+import argparse
+import time
 
-checkpoint = "Human_play_Nav_" + datetime.now().isoformat()[:10] + "_" + datetime.now().isoformat()[
-                                                                     11:13] + "-" + datetime.now().isoformat()[
-                                                                                    14:16] + ".pth.tar"
-device = torch.device('cuda:' + str(param["device_idx"][0]) if torch.cuda.is_available() else 'cpu')
-unity_env = UnityEnvironment(param["env_path"])
-env = MultiUnityWrapper(unity_env=unity_env, uint8_visual=True, allow_multiple_obs=True)
-agent_ids = tuple(env.agent_id_to_behaviour_name.keys())
-max_steps = param["max_steps"]
-mem = Memory(memsize=param["total_episodes"], agent_ids=agent_ids)
-exit = False
-for _ in tqdm(range(param["total_episodes"])):
-    prev_obs = env.reset()
-    step_count = 0
-    local_memory = {}
-    for id in agent_ids:
-        local_memory[id]=[]
-    done = False
-    while step_count < max_steps and not done:
+from unity_wrappers.envs import MultiUnityWrapper
+from mlagents_envs.environment import UnityEnvironment
+
+import _pickle as cPickle
+from utils import save_checkpoint
+from Experience_Replay import Memory
+
+class Human_play:
+    def __init__(
+            self,
+            id_to_name,
+            device_idx=0,
+
+    ):
+        self.id_to_name = id_to_name
+        self.agent_ids = tuple(id_to_name.keys())
+        self.device_idx = device_idx
+        self.device = torch.device('cuda:' + str(device_idx) if torch.cuda.is_available() else 'cpu')
+    def initialize_env(self, env_path, max_steps, total_episodes):
+        unity_env = UnityEnvironment(env_path)
+        self.env = MultiUnityWrapper(unity_env=unity_env, uint8_visual=True, allow_multiple_obs=True)
+        self.max_steps=max_steps
+        self.total_episodes=total_episodes
+    def controller(self):
         act = {}
         # {0: agent 0's action, 1: ...]
-        for id in agent_ids:
+        for id in self.agent_ids:
             act[id] = np.zeros(2)
         if keyboard.is_pressed("down"):
             act[1][0] = -1
@@ -61,24 +64,96 @@ for _ in tqdm(range(param["total_episodes"])):
         #     act[0][1] = -1
         # else:
         #     act[0][1] = 0
-        if keyboard.is_pressed("esc"):
-            exit = True
-            break
-        obs_dict, reward_dict, done_dict, info_dict = env.step(act)
-        step_count += 1
-        done = done_dict["__all__"]
-        for id in agent_ids:
-            local_memory[id].append(
-                (deepcopy(prev_obs[id]), deepcopy(act[id]), deepcopy(reward_dict[id]), deepcopy(obs_dict[id])))
-        prev_obs = obs_dict
-    if exit:
-        save_checkpoint({"memory": mem}, filename=checkpoint)
-        env.close()
-        break
+        return act
+    def play_game(self, real_time_memory_sync=False, hostname='localhost'):
+        if real_time_memory_sync:
+            self.initialize_server(hostname)
+        mem = Memory(memsize=self.total_episodes, agent_ids=self.agent_ids)
+        exit = False
+        for _ in tqdm(range(self.total_episodes)):
+            prev_obs = self.env.reset()
+            step_count = 0
+            local_memory = {}
+            for id in self.agent_ids:
+                local_memory[id] = []
+            done = False
+            while step_count < self.max_steps and not done:
+                act=self.controller()
+                if keyboard.is_pressed("esc"):
+                    exit = True
+                    break
+                obs_dict, reward_dict, done_dict, info_dict = self.env.step(act)
+                step_count += 1
+                done = done_dict["__all__"]
+                for id in self.agent_ids:
+                    local_memory[id].append(
+                        (deepcopy(prev_obs[id]), deepcopy(act[id]), deepcopy(reward_dict[id]), deepcopy(obs_dict[id])))
+                prev_obs = obs_dict
+            mem.add_episode(local_memory)
+            if real_time_memory_sync:
+                self.update(local_memory)
+            if exit:
+                break
+        self.env.close()
+        return mem
+    def initialize_server(self, hostname='localhost'):
+        self.connect=redis.Redis(host=hostname)
+        self.connect.delete("experience")
+    def real_time_mode(self, hostname='localhost'):
+        self.play_game(real_time_memory_sync=True, hostname=hostname)
+    def save_mode(self,checkpoint_name):
+        mem=self.play_game()
+        filename = checkpoint_name + "_"+ datetime.now().isoformat()[:10] + "_" + datetime.now().isoformat()[
+                                                                     11:13] + "-" + datetime.now().isoformat()[
+                                                                                    14:16] + ".pth.tar"
+        save_checkpoint({"memory": mem}, filename=filename)
 
-    mem.add_episode(local_memory)
-if not exit:
-    save_checkpoint({"memory": mem}, filename=checkpoint)
-    env.close()
-filepath = os.path.join('Checkpoint', checkpoint)
-checkpoint = torch.load(filepath, device)
+    def load_mode(self, checkpoint_name, hostname='localhost'):
+        self.initialize_server(hostname)
+        filepath=os.path.join('Checkpoint', checkpoint_name)
+        checkpoint = torch.load(filepath, self.device)
+        memory=checkpoint["memory"]
+        for i in range(len(memory)):
+            local_memory = {}
+            for id in self.agent_ids:
+                local_memory[id] = memory.memory[id][i]
+            self.update(local_memory)
+    def update(self, local_memory):
+        self.connect.rpush("experience", cPickle.dumps(local_memory))
+        with self.connect.lock("Update"):
+            self.wait_until_present("episode_count")
+            episode_count = cPickle.loads(self.connect.get("episode_count"))
+            episode_count = episode_count + 1
+            self.connect.set("episode_count", cPickle.dumps(episode_count))
+    def wait_until_present(self, name):
+        # print("Waiting for "+name)
+        while True:
+            if not self.connect.get(name) is None:
+                # print(name+" received")
+                break
+            time.sleep(0.1)
+
+if __name__=="__main__":
+    parser = argparse.ArgumentParser(description='Learner process for distributed reinforcement.')
+    parser.add_argument('-m', '--mode', type=str, default='l', help="Runing mode. s:save_mode, l:load_mode, r: real time mode")
+    args=parser.parse_args()
+    with open("config/Human_Play.yaml") as file:
+        param = yaml.safe_load(file)
+    env_path=param["env_path"]
+    total_episodes=param["total_episodes"]
+    max_steps = param["max_steps"]
+    device_idx = param["device_idx"]
+    id_to_name=param["id_to_name"]
+    hostname = param["hostname"]
+    human_play=Human_play(id_to_name=id_to_name, device_idx=device_idx)
+    if args.mode=='l':
+        checkpoint_to_load = param["checkpoint_to_load"]
+        human_play.load_mode(checkpoint_name=checkpoint_to_load, hostname=hostname)
+    else:
+        human_play.initialize_env(env_path=env_path,max_steps=max_steps,total_episodes=total_episodes)
+        if args.mode=='s':
+            checkpoint_save_name=param["checkpoint_save_name"]
+            human_play.save_mode(checkpoint_name=checkpoint_save_name)
+        else:
+            human_play.real_time_mode(hostname)
+        
