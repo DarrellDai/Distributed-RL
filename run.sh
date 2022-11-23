e3503e47873d269b909b93a9c5ae249b80d48af9
sd469 <daishiqin1996@gmail.com>
2022-11-23 18:09:11 -0500

    Update configs for BC

--- run.sh
+++ run.sh
@@ -17,21 +17,21 @@ echo "redis server:" $redis_server
 
 pids="" 
 if $human_play; then
-    mpirun -np $num_learners python Learner.py -rc $Train_human_play_config &
+    mpirun -np $num_learners python Learner.py -rc $Train_human_play_config -ins $instance_idx &
     pids="$pids $!"
-	python Human_play.py -m l &
+	python Human_play.py -m l -ins $instance_idx &
 	pids="$pids $!"
 fi
 wait -f $pids
-pids=""
-if $leaner; then
-	mpirun -np $num_learners python Learner.py -rc $Train_config -ins $instance_idx &
-	pids="$pids $!"
-fi
-
-if $actor; then
-    python Actor.py -n $num_actors -rc $Train_config -r $redis_server -d $device -ins $instance_idx &
-    pids="$pids $!"
-fi
-# Exit the this script when there's a child process is done, so all child process will be killed
-wait -n $pids
+#pids=""
+#if $leaner; then
+#	mpirun -np $num_learners python Learner.py -rc $Train_config -ins $instance_idx &
+#	pids="$pids $!"
+#fi
+#
+#if $actor; then
+#    python Actor.py -n $num_actors -rc $Train_config -r $redis_server -d $device -ins $instance_idx &
+#    pids="$pids $!"
+#fi
+## Exit the this script when there's a child process is done, so all child process will be killed
+#wait -n $pids
